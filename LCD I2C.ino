1368149654c7f04ede34ead8192657c69cc71715
Igor Gabriel <107767224+IgorGabrielDs@users.noreply.github.com>
2025-06-02 21:23:30 -0300

    Update LCD I2C.ino

--- LCD I2C.ino
+++ LCD I2C.ino
@@ -33,118 +33,60 @@ void Texto(LiquidCrystal_I2C &lcd, int a, int b, const char* texto1, int c, int 
 
 void loop() {
   Texto(lcd1, 0, 0, string[1], 1, 1, string[2]); // "Hora de navegar!" e "Todos a bordo?"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[3], 1, 1, string[4]); // "Esconda navios" e "na zona rival"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 2, 0, string[5], 0, 1, string[6]); // "Posicione o" e "barco de 3 casas"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[7], 0, 1, string[8]); // "Toque uma casa" e "amarela inicial!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[9], 2, 1, string[10]); // "Toque na segunda" e "casa amarela"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[11], 3, 1, string[12]); // "Escolha o fim" e "do barco 3"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 2, 0, string[5], 0, 1, string[13]); // "Posicione o" e "barco de 2 casas"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[7], 0, 1, string[8]); // "Toque uma casa" e "amarela inicial!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[11], 3, 1, string[14]); // "Escolha o fim" e "do barco 2"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 2, 0, string[5], 0, 1, string[15]); // "Posicione o" e "barco de 1 casa!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[16], 2, 1, string[17]); // "Selecione uma" e "casa amarela"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[18], 2, 1, string[19]); // "Duas escolhas" e "te aguardam:"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[20], 0, 1, string[21]); // "Clique no barco" e "e mude de lugar"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[22], 0, 1, string[23]); // "ou aperte duas" e "vezes para jogar"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[24], 1, 1, string[25]); // "Barco cancelado!" e "Vamos de novo"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[26], 2, 1, string[27]); // "Hora de iniciar" e "a pontaria!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[28], 4, 1, string[29]); // "Sua vez de jogar" e "marujo!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[30], 0, 1, string[31]); // "Respire fundo..." e "vez do oponente"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 2, 0, string[32], 5, 1, string[33]); // "Sem sorte..." e "Agua!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[34], 0, 1, string[35]); // "Acertou o trecho" e "do navio inimigo"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[36], 0, 1, string[37]); // "Bom tiro, marujo" e "acertou o barco!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[38], 1, 1, string[39]); // "Acertaram parte" e "do seu navio!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[40], 2, 1, string[41]); // "O inimigo achou" e "o seu barco!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 1, 0, string[42], 2, 1, string[43]); // "Jogue de novo!" e "Boa jogada!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 2, 0, string[44], 0, 1, string[45]); // "Essa casa ta" e "ocupada, marujo!"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[46], 0, 1, string[47]); // "Bom jogo! Vamos" e "jogar de novo?"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[48], 2, 1, string[49]); // "Vitoria, marujo!" e "Bom trabalho"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 0, 0, string[50], 2, 1, string[51]); // "Tente novamente:" e "Casa ocupada"
-  delay(2000);
-  lcd1.clear();
   
   Texto(lcd1, 2, 0, string[52], 0, 1, string[53]); // "Um clique de" e "cada vez marujo!"
-  delay(2000);
-  lcd1.clear();
 }
